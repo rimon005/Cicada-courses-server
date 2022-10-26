8a34a6a6edefa765eec95b36d62be450e969eafb
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-26 16:07:51 +0600

    added category and courses api

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json')
+
+const courses = require('./data/courses.json')
 
 app.get('/' , (req , res) =>{
     res.send('Courses api running')
+})
+
+app.get('/category' ,(req , res) =>{
+    res.send(categories)
+})
+
+app.get('/courses' , (req , res) =>{
+    res.send(courses)
 })
 
 app.listen(port , ()=>{
