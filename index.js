aa74b0a8b1d3bde4ea47bcecead88fa4edf1875b
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-10-26 21:35:32 +0600

    handle error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -9,30 +9,37 @@ const categories = require('./data/categories.json')
 
 const courses = require('./data/courses.json')
 
-app.get('/' , (req , res) =>{
-    res.send('Courses api running')
+app.get('/', (req, res) => {
+    res.send('News API Running')
 })
 
-app.get('/category' ,(req , res) =>{
+app.get('/category', (req, res) => {
     res.send(categories)
+});
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses)
+    }
+    else {
+        const categoryCourse = courses.filter(n => n.category_id === id);
+        res.send(categoryCourse)
+    }
 })
 
-app.get('/category/:id' , (req , res) =>{
-    const id = req.params.id;
-    const categoryCourse = courses.find( c => c.category_id === id)
-    res.send(categoryCourse)
-} )
-
-app.get('/courses' , (req , res) =>{
+app.get('/courses' , (req , res ) =>{
     res.send(courses)
 })
 
-app.get('/courses/:id' , (req , res)=>{
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourse = courses.find( c => c._id === id);
+    const selectedCourse = courses.find(n => n._id === id);
     res.send(selectedCourse)
 })
 
-app.listen(port , ()=>{
-    console.log('Cicada courses server in running' );
-})
+
+app.listen(port, () => {
+    console.log('Cicada Server  running on port', port)
+});
